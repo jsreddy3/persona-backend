b5df9d3556bb2756df06e36c0275c62658122efb
Vivek Vajipey <vivek.vajipey@gmail.com>
2025-02-24 15:42:18 -0300

    penpals

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ app.add_middleware(
         "https://persona-ai.vercel.app",
         "https://backend-persona.herokuapp.com",
         "https://backend-persona-da6c29e3bf72.herokuapp.com",
-        "https://frontend-persona-nine.vercel.app"
+        "https://frontend-persona-nine.vercel.app",
+        "http://penpals.cloud",
+        "https://penpals.cloud"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
